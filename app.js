51d99716c7eea09e4a154687cddb7d79e0e399ef
Lauro Rodríguez <laurorodven@gmail.com>
2025-01-20 07:36:52 -0600

    Creación funciones limpiarCampo y agregarAmigo

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 //Creamos el array para almacenar los nombres
 
-let nombresAmigos = []
+let nombresAmigos = [];
+
+function limpiarCampo(elemento) {
+    document.querySelector(elemento).value = '';
+    return;
+}
+
+function agregarAmigo() {
+    let campoTexto = document.querySelector('#amigo').value;
+    if (campoTexto == '') {
+        alert('Por favor, inserte un nombre para poder continuar');
+    }
+    else {
+        if (!nombresAmigos.includes(campoTexto)) {
+            nombresAmigos.push(campoTexto);
+        }
+        else {
+            alert('Ese nombre ya fue agregado, ingresa otro');
+        }
+    }
+    console.log(nombresAmigos);
+    limpiarCampo('#amigo');
+    return;
+}
+
